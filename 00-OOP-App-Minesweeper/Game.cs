7b8417c31c994640ab768cb31d07a7acb9271b42
Jan Koupil <jan.koupil@gmail.com>
2023-03-01 10:33:19 +0100

    2023-03-01 Pocitani sousedu

--- 00-OOP-App-Minesweeper/Game.cs
+++ 00-OOP-App-Minesweeper/Game.cs
@@ -15,7 +15,7 @@ namespace _00_OOP_App_Minesweeper
 
         public Game(int width, int height, int mineCnt)
         {
-            //mělo by být hlídání neplatných - záporných hodnot a exception
+            //mělo by zde být hlídání neplatných - záporných hodnot a exception
             this.width = width;
             this.height = height;
             this.mineCnt = mineCnt;
@@ -37,7 +37,80 @@ namespace _00_OOP_App_Minesweeper
                 }
             }
 
-            //ToDo: Randomize array
+            RandomizeMap();
+            StoreNeighbourCount();
         }
+
+        private void RandomizeMap ()
+        {
+            //projit cele pole
+            //ke kazdemu prvku vytvorit nahodne souradnice a prohdodit
+
+            Random rnd = new Random();
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int targetX = rnd.Next(width);
+                    int targetY = rnd.Next(height);
+
+                    Tile tmp = tiles[targetX, targetY];
+                    tiles[targetX, targetY] = tiles[x, y];
+                    tiles[x, y] = tmp;
+
+                    //(tiles[x, y], tiles[targetX, targetY]) = (tiles[targetX, targetY], tiles[x, y]);
+                    //jiny zpusob prohozeni - pomoci tuple
+                }
+            }
+        }
+        /// <summary>
+        /// For each tile calculates neighbouring mines and stores them
+        /// </summary>
+        private void StoreNeighbourCount()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Tile tile = tiles[x, y];
+                    tile.Neighbours = CountNeighbours(x, y);
+                }
+            }
+        }
+
+        /// <summary>
+        /// calculates neighbour count for a single tile
+        /// </summary>
+        private int CountNeighbours(int x, int y)
+        {
+            int count = 0;
+
+            for (int shiftX = -1; shiftX < 2; shiftX++)
+            {
+                for (int shiftY = -1; shiftY < 2; shiftY++)
+                {
+                    if (shiftX == 0 && shiftY == 0) //nepocitej stred
+                        continue;
+
+                    int neighbourX = x + shiftX;
+                    int neighbourY = y + shiftY;
+
+                    if (
+                        neighbourX > -1 && neighbourX < width
+                        && neighbourY > -1 && neighbourY < height
+                        && tiles[x, y].IsMine
+                    )
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        public void Run()
+        {
+
+        }
+
     }
 }
--- 00-OOP-App-Minesweeper/Tile.cs
+++ 00-OOP-App-Minesweeper/Tile.cs
@@ -79,6 +79,5 @@ namespace _00_OOP_App_Minesweeper
             }
         }
 
-
     }
 }
